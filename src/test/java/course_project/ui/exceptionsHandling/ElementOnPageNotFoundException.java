b8ebdbf16eb7d1c1fbcea5e6d557957f2f3ee627
Dmytro Hryniuk <dmitriy.hryniuk@gmail.com>
2024-01-27 20:07:16 +0200

    Quick-Fixes

---
+++ src/test/java/course_project/ui/exceptionsHandling/ElementOnPageNotFoundException.java
@@ -0,0 +1,8 @@
+package course_project.ui.exceptionsHandling;
+
+public class ElementOnPageNotFoundException extends RuntimeException {
+    public ElementOnPageNotFoundException(String elementName, String pageName) {
+        super(String.format("\nElement with name '%s' was not found. " +
+                "Check element in '%s' element name mapper", elementName, pageName));
+    }
+}
---
+++ src/test/java/course_project/ui/exceptionsHandling/PageNotFoundException.java
@@ -0,0 +1,7 @@
+package course_project.ui.exceptionsHandling;
+
+public class PageNotFoundException extends RuntimeException {
+    public PageNotFoundException(String pageTypeName, String allPages) {
+        super(String.format("Page with name '%s' was not found. Current pages: %n%s", pageTypeName, allPages));
+    }
+}

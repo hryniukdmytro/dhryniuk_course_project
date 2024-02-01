05205e5a8618e72bbaf83b7029504d6a74e08c54
Dmytro Hryniuk <dmitriy.hryniuk@gmail.com>
2024-02-01 19:54:22 +0200

    Refactor

---
+++ JenkinsFile.groovy
@@ -0,0 +1,19 @@
+pipeline {
+
+    tools {
+        maven 'Maven'
+        jdk 'JDK_17'
+    }
+
+    agent any
+
+    stages {
+        stage('Build and Run Tests') {
+            steps {
+                script {
+                    sh 'mvn clean test -DSuite=allTests'
+                }
+            }
+        }
+    }
+}

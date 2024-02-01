81dfad4887a0c40b2ce3a15029ea5da7097694aa
Dmytro Hryniuk <dmitriy.hryniuk@gmail.com>
2024-02-01 20:16:33 +0200

    Refactor2

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,17 +1,25 @@
 pipeline {
-
-    tools {
-        maven 'Maven'
-        jdk 'JDK_17'
-    }
-
     agent any
+
+    parameters {
+        choice(name: 'testSuite',
+                choices:
+                        ['allTests',
+                         'onlyAPITests',
+                         'onlyUITests',
+                         'projectTests',
+                         'taskTests',
+                         'userTests'],
+                description:
+                        'Select the test suite to execute'
+        )
+    }
 
     stages {
         stage('Build and Run Tests') {
             steps {
                 script {
-                    sh 'mvn clean test -Dsuite=onlyAPITests'
+                    sh "mvn clean test -Dsuite=${params.testSuite}"
                 }
             }
         }
